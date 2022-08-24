e1191364e73cdcbfd4134bd61991dc6eb9f20df9
Seggi SMS <seggimarugira@gmail.com>
2022-08-24 12:45:28 +0200

    add models

--- src/db/model/index.ts
+++ src/db/model/index.ts
@@ -1,5 +1,13 @@
+import Advices from "./Advice";
+import Foods from "./foods";
+import Medicines from "./medicines";
+import Sickness from "./sickness";
 import Users from "./users";
 
 export default {
     users: Users,
+    medicines: Medicines,
+    sickness: Sickness,
+    advices: Advices,
+    foods: Foods,
 }
---
+++ src/db/model/sickness.ts
@@ -0,0 +1,52 @@
+import { DataTypes, Model, Optional } from 'sequelize';
+import connect from "../db.config";
+import Advices from './Advice';
+import Foods from './foods';
+import Medicines from './medicines';
+import Users from './users';
+
+interface SicknessAttributes {
+    id: number;
+    name: string;
+    description: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+    deletedAt?: Date;
+}
+
+export type sicknessInput = Optional<SicknessAttributes, 'id'>
+export type sicknessOutput = Required<SicknessAttributes>
+
+class Sickness extends Model {
+    public id!:  number;
+    public name!: string;
+    public description!: string;
+    public readonly createAt!: Date | undefined;
+    public readonly updateAt!: Date | undefined;
+    public readonly deleteAt!: Date | undefined;
+}
+
+Sickness.init ({
+    id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+},  {
+    timestamps: true,
+    paranoid: true,
+    sequelize: connect()
+})
+
+Sickness.belongsTo(Users)
+Users.hasMany(Sickness)
+
+export default Sickness;
